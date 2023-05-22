7451f0f2850d623caa96d65dcf63a22da6cebae3
Gotine31 <gotine31@outlook.com>
2023-05-22 16:59:01 -0300

    sei lá

---
+++ calculadoraDesafio.java
@@ -0,0 +1,20 @@
+public class calculadoraDesafio {
+    public static void main(String[] args) {
+        calculadora(14, 28);
+
+    }
+    public static void calculadora(double x, double y) {
+        System.out.println("Soma : ");
+        System.out.println(x + y);
+
+        System.out.println("Subtração : ");
+        System.out.println(x - y);
+
+        System.out.println("Multiplicação : ");
+        System.out.println(x * y);
+
+        System.out.println("Divisão : ");
+        System.out.println(x / y);
+
+    }
+}
